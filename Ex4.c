b65b421b33805b524f3556736951ecdf49196e4c
mukesh02032007 <mukeshp.240193@aids.ritchennai.edu.in>
2026-02-11 16:23:15 +0530

    Update Ex4.c

--- Ex4.c
+++ Ex4.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <time.h>
-
 #define MAX 10000
-
 // ---------- INSERTION SORT ----------
 void insertionSort(int arr[], int n)
 {
     int i, key, j;
-
     for (i = 1; i < n; i++)
     {
         key = arr[i];
         j = i - 1;
-
         while (j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j];
@@ -21,7 +17,6 @@ void insertionSort(int arr[], int n)
         arr[j + 1] = key;
     }
 }
-
 // ---------- HEAPIFY ----------
 void heapify(int arr[], int n, int i)
 {
@@ -29,43 +24,34 @@ void heapify(int arr[], int n, int i)
     int left = 2 * i + 1;
     int right = 2 * i + 2;
     int temp;
-
     if (left < n && arr[left] > arr[largest])
         largest = left;
-
     if (right < n && arr[right] > arr[largest])
         largest = right;
-
     if (largest != i)
     {
         temp = arr[i];
         arr[i] = arr[largest];
         arr[largest] = temp;
-
         heapify(arr, n, largest);
     }
 }
-
 // ---------- HEAP SORT ----------
 void heapSort(int arr[], int n)
 {
     int i, temp;
-
     // Build heap
     for (i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
-
     // Extract elements
     for (i = n - 1; i > 0; i--)
     {
         temp = arr[0];
         arr[0] = arr[i];
         arr[i] = temp;
-
         heapify(arr, i, 0);
     }
 }
-
 // ---------- MAIN ----------
 int main()
 {
@@ -73,32 +59,25 @@ int main()
     int n, i;
     clock_t start, end;
     double time_taken;
-
     printf("Enter number of elements: ");
     scanf("%d", &n);
-
     printf("Enter elements:\n");
     for (i = 0; i < n; i++)
     {
         scanf("%d", &arr1[i]);
         arr2[i] = arr1[i]; // Copy for heap sort
     }
-
     // ----- Insertion Sort Time -----
     start = clock();
     insertionSort(arr1, n);
     end = clock();
-
     time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("\nInsertion Sort Time = %f seconds\n", time_taken);
-
     // ----- Heap Sort Time -----
     start = clock();
     heapSort(arr2, n);
     end = clock();
-
     time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("Heap Sort Time = %f seconds\n", time_taken);
-
     return 0;
 }
