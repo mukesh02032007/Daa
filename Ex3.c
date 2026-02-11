01ed3079b23bd56aa6c8eb0610a646e6fc5c253f
mukesh02032007 <mukeshp.240193@aids.ritchennai.edu.in>
2026-02-11 08:53:47 +0530

    Add pattern searching function in Ex3.c
    Implement a pattern searching algorithm in C.

---
+++ Ex3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+void search(char pat[], char txt[])
+{
+    int i, j;
+    int n = strlen(txt);
+    int m = strlen(pat);
+
+    // Loop through text
+    for (i = 0; i <= n - m; i++)
+    {
+        // Check pattern match
+        for (j = 0; j < m; j++)
+        {
+            if (txt[i + j] != pat[j])
+                break;
+        }
+
+        // If pattern found
+        if (j == m)
+        {
+            printf("Pattern found at index %d\n", i);
+        }
+    }
+}
+
+int main()
+{
+    char txt[100], pat[100];
+
+    printf("Enter the text: ");
+    gets(txt);
+
+    printf("Enter the pattern: ");
+    gets(pat);
+
+    search(pat, txt);
+
+    return 0;
+}
